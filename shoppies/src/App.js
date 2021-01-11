a8a4ea67da6b6024206f09941da5f6d2d8009cef
wendy-guo <wendy.guo@live.com>
2021-01-10 22:04:55 -0500

    implemented opening/closing of SideBar

--- shoppies/src/App.js
+++ shoppies/src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
+import { useState } from "react";
 import TopBar from "./components/TopBar";
 import SearchBar from "./components/SearchBar";
+import SideBar from "./components/SideBar";
 
 function App() {
+  const [showSideBar, setShowSideBar] = useState(false);
+
+  const handleNomClick = () => {
+    setShowSideBar(true);
+  };
+
+  const handleCloseSidebar = () => {
+    setShowSideBar(false);
+  };
+
   return (
     <div className="App">
-      <TopBar />
+      <TopBar onNomClick={handleNomClick} />
       <SearchBar />
+      <SideBar
+        numNom={0}
+        nominations={[1, 2, 3, 4, 5]}
+        open={showSideBar}
+        onClose={handleCloseSidebar}
+      />
     </div>
   );
 }
