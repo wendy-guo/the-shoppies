9c785865aa247770d5f506cdfc5601228f849d1b
wendy-guo <wendy.guo@live.com>
2021-01-11 19:30:31 -0500

    displayed movie info

--- shoppies/src/components/Movies/index.js
+++ shoppies/src/components/Movies/index.js
@@ -1,11 +1,24 @@
 import "./style.css";
 
 function Movies(props) {
-  return <div></div>;
+  let movies = props.movies || [];
+  return (
+    <div>
+      {movies.map((movie, i) => (
+        <Movie
+          key={100 + i}
+          id={movie.imdbID}
+          title={movie.Title}
+          year={movie.Year}
+          poster={movie.Poster}
+        />
+      ))}
+    </div>
+  );
 }
 
 function Movie(props) {
-  return <div></div>;
+  return <div className="movie">{props.title}</div>;
 }
 
 export default Movies;
