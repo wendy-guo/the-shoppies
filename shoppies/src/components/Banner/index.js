a14a1c1b84e01e8a92b7817c170c161d9f0689df
wendy-guo <wendy.guo@live.com>
2021-01-13 11:18:25 -0500

    created Banner component

---
+++ shoppies/src/components/Banner/index.js
@@ -0,0 +1,20 @@
+import "./style.css";
+ 
+function Banner(props) {
+  return (
+    <div
+      className="banner"
+      style={{
+        transform: props.show ? "translateY(0px)" : "translateY(60px)",
+      }}
+    >
+      <div className="close-btn" onClick={props.onClose}>
+        <IoMdClose />
+      </div>
+      <div>You have nominated 5 movies!</div>
+      <div className="sub-btn">submit</div>
+    </div>
+  );
+}
+
+export default Banner;
